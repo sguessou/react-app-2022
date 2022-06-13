6930514736e8635a4a39ea68a4762561bbeeb605
Saad Guessous <guessous_saad@hotmail.com>
2022-06-13 22:39:01 +0300

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/sguessou/react-app-2022",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
